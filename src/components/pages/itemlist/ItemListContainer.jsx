c167586f8c3d827fe59e757ef23f2f87e2ae1817
mariade_snchz <calder07@gmail.com>
2023-08-30 14:29:08 -0300

    card responsive

--- src/components/pages/itemlist/ItemListContainer.jsx
+++ src/components/pages/itemlist/ItemListContainer.jsx
@@ -21,32 +21,31 @@ const ItemListContainer = () => {
 
 
   return (
-    <div>
-    <h1>Estoy en el shop</h1>
-  
-    {products.map((product) => {
-      return (
-        <div 
-          key={product.id}
-          style={{
-            border: "1px solid black",
-            width: "25%", // Para tener 4 elementos por fila en pantallas grandes
-            boxSizing: "border-box", // Considerar el borde en el ancho total
-            padding: "20px", // Espaciado entre elementos
-          
-          }}
-        >
-          <img src={product.image} style={{ width: "100%" }} alt="" />
-          <h4>{product.title}</h4>
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "20px", justifyContent: "center" }}>
+  <h1>Estoy en el shop</h1>
 
-          <h4>{product.unit_price}</h4>
-          <h4>{product.stock}</h4>
-          <Link to={`/itemDetail/${product.id}`}>Ver detalle</Link>
-        </div>
-      );
-    })}
-  </div>
-  
+  {products.map((product) => {
+    return (
+      <div 
+        key={product.id}
+        style={{
+          border: "1px solid black",
+          flex: "1 1 calc(25% - 40px)", // Calcula el ancho para 4 elementos por fila con espacio
+          padding: "20px",
+          boxSizing: "border-box",
+          minWidth: "250px", // Ancho mínimo para evitar elementos demasiado pequeños
+        }}
+      >
+        <img src={product.image} style={{ width: "100%" }} alt="" />
+        <h4>{product.title}</h4>
+        <h4>{product.unit_price}</h4>
+        <h4>{product.stock}</h4>
+        <Link to={`/itemDetail/${product.id}`}>Ver detalle</Link>
+      </div>
+    );
+  })}
+</div>
+
 
   );
 };
