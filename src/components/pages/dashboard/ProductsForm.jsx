4b7e8e0907e56ab8e2636766b68fc69bc20e83fb
mariade_snchz <calder07@gmail.com>
2023-08-30 15:23:14 -0300

    actualiz

--- src/components/pages/dashboard/ProductsForm.jsx
+++ src/components/pages/dashboard/ProductsForm.jsx
@@ -19,8 +19,8 @@ const ProductsForm = ({
     category: "",
     image: "",
   });
-  const [urls, setUrls] = useState([])
-  const [files, setFiles] = useState([]);
+  const [url, setUrl] = useState(null)
+  const [file, setFile] = useState(null);
 
   const handleImage = async () => {
     setIsLoading(true);
@@ -135,7 +135,7 @@ const ProductsForm = ({
                   />
       
         </div>
-        <TextField type="file" onChange={(e) => setFiles(e.target.files[0])} multiple />
+        <TextField type="file" onChange={(e) => setFile(e.target.files[0])} multiple />
         {file && (
           <Button onClick={handleImage} type="button">
             Cargar imagen
