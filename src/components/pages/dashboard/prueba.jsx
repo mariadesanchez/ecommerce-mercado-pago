c3a3c9163e3a0975ff1de944fad63cb0e6cb9150
mariade_snchz <calder07@gmail.com>
2023-09-09 11:46:39 -0300

    Edit userOrder

---
+++ src/components/pages/dashboard/prueba.jsx
@@ -0,0 +1,43 @@
+<TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {/* <TableCell align="left">id</TableCell> */}
+              <TableCell align="left">Producto</TableCell>
+              <TableCell align="left">Precio</TableCell>
+              <TableCell align="left">imagen</TableCell>
+             
+            
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow
+                key={product.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+               
+                <TableCell component="th" scope="row" align="left">
+                  {product.title}
+                </TableCell>
+
+                <TableCell component="th" scope="row" align="left">
+                  {product.unit_price}
+                </TableCell>
+        
+                <TableCell component="th" scope="row" align="left">
+                  <img
+                    src={product.image}
+                    alt=""
+                    style={{ width: "80px", height: "80px" }}
+                  />
+                </TableCell>
+                <TableCell component="th" scope="row" align="left">
+                  {product.category}
+                </TableCell>
+               
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
--- src/components/pages/userOrders/UserOrders.jsx
+++ src/components/pages/userOrders/UserOrders.jsx
@@ -3,6 +3,13 @@ import { useState } from "react"
 import { db } from "../../../firebaseConfig"
 import {getDocs, collection, query, where} from "firebase/firestore"
 import { AuthContext } from "../../../context/AuthContext"
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const UserOrders = () => {
 
@@ -26,22 +33,88 @@ const UserOrders = () => {
 console.log(myOrders)
   return (
     <div>
-        <h1>estoy en mis ordenes de compras</h1>
+        {/* <h1>estoy en mis ordenes de compras</h1>
         {
-          myOrders?.map( order => {
+          myOrders.map( order => {
             return <div key={order.id} style={{border:"2px solid black"}}>
               {
                 order?.items?.map( product => {
                   return <div key={product.id}>
                       <h2>{product.title}</h2>
                       <h3>{product.quantity}</h3>
+                    
                   </div>
                 })
               }
               <h4>El total de la orden es {order.total}</h4>
             </div>
           })
+        } 
+        */}
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {/* <TableCell align="left">id</TableCell> */}
+              <TableCell align="left">Producto</TableCell>
+              <TableCell align="left">Cantidad</TableCell>
+              <TableCell align="left">Precio</TableCell>
+              <TableCell align="left">Total</TableCell>
+              <TableCell align="left">imagen</TableCell>
+             
+            
+            </TableRow>
+          </TableHead>
+          <TableBody>
+         { myOrders.map( order => {
+            return <div key={order.id} style={{border:"2px solid black"}}>
+              {
+                order?.items?.map( product => {
+                  return <div key={product.id}>
+        
+              <TableRow
+                key={product.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+               
+                <TableCell component="th" scope="row" align="left">
+                  {product.title}
+                </TableCell>
+
+                <TableCell component="th" scope="row" align="left">
+                  {product.quantity}
+                </TableCell>
+
+                <TableCell component="th" scope="row" align="left">
+                  {product.unit_price}
+                </TableCell>
+        
+                <TableCell component="th" scope="row" align="left">
+                  {product.total}
+                </TableCell>
+
+                <TableCell component="th" scope="row" align="left">
+                  <img
+                    src={product.image}
+                    alt=""
+                    style={{ width: "80px", height: "80px" }}
+                  />
+                </TableCell>
+              
+               
+              </TableRow>
+               
+              </div>
+                })
+              }
+              {/* <h4>El total de la orden es {order.total}</h4> */}
+            </div>
+          })
         }
+        
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
